5ca484c4894ed1e02d578dc182acec7e02a03d3f
Agustin Cao <alcaolpg@gmail.com>
2023-02-16 20:10:36 -0300

    code refactor tool interface

--- Assets/Scripts/ToolsMainMode.cs
+++ Assets/Scripts/ToolsMainMode.cs
@@ -43,13 +43,8 @@ public class ToolsMainMode : MonoBehaviour
         }
         imageManager.trackedImagesChanged += OnTrackedImageChanged;
 
-        toolName.text = "";
-        infoButton.interactable = false;
-        animationButton.interactable = false;
-        animationButtonText.text = "";
-        infoButtonText.text = "";
-        infoButtonLogo.text = "";
         detailsPanel.SetActive(false);
+        disableToolInterface();
     }
 
     private void OnDisable()
@@ -93,34 +88,43 @@ public class ToolsMainMode : MonoBehaviour
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
             {
                 Tool tool = hit.collider.GetComponentInParent<Tool>();
-                toolName.text = tool.toolName;
-                detailsText.text = tool.toolDescription;
-                infoButton.interactable = true;
-                animationButton.interactable = true;
-                infoButtonText.text = "Información";
-                infoButtonLogo.text = "?";
-                if (tool.isOpen)
-                    animationButtonText.text = "Cerrar";
-                else
-                    animationButtonText.text = "Abrir";
+                enableToolInterface(tool);
                 
             }
             else
             {
-                if (detailsPanel.activeInHierarchy == false)
-                {
-                    toolName.text = "";
-                    detailsText.text = "";
-                    animationButtonText.text = "";
-                    infoButtonText.text = "";
-                    infoButtonLogo.text = "";
-                    infoButton.interactable = false;
-                }
-                animationButton.interactable = false;
+                disableToolInterface();
             }  
         }
     }
 
+    private void enableToolInterface(Tool tool)
+    {
+        toolName.text = tool.toolName;
+        detailsText.text = tool.toolDescription;
+        infoButton.interactable = true;
+        animationButton.interactable = true;
+        infoButtonText.text = "Información";
+        infoButtonLogo.text = "?";
+        if (tool.isOpen)
+            animationButtonText.text = "Cerrar";
+        else
+            animationButtonText.text = "Abrir";
+    }
+
+    private void disableToolInterface()
+    {
+        if (detailsPanel.activeInHierarchy == false)
+        {
+            toolName.text = "";
+            detailsText.text = "";
+            infoButtonText.text = "";
+            infoButtonLogo.text = "";
+            infoButton.interactable = false;
+        }
+        animationButton.interactable = false;
+        animationButtonText.text = "";
+    }
     public void ToggleAnimation()
     {
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
